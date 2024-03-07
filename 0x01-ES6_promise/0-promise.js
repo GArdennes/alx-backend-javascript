97ba0daebc3a5d156e093e5172c718777cf2d5cf
GArdennes <thekevin.afachao@gmail.com>
2024-03-07 12:07:11 +0000

    Task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,4 @@
-export function getResponseFromAPI() {
-  const promise1 = new Promise(resolve => setTimeout(() => resolve(1), 1000));
+export default function getResponseFromAPI() {
+  const promise1 = new Promise((resolve) => setTimeout(() => resolve(1), 1000));
   return promise1;
 }
-
