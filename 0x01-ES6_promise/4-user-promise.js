bb5ba14205c4b90ba109251f0ee5d27608938e4a
GArdennes <thekevin.afachao@gmail.com>
2024-03-07 12:35:30 +0000

    Task 4|

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,3 @@
-export function signUpUser(firstName, lastName) {
-  return Promise.resolve({{firstName: `${firstName}`, lastName: `${lastName}`}});
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({ firstName: `${firstName}`, lastName: `${lastName}` });
 }
