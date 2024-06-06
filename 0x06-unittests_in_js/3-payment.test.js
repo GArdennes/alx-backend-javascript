23a4c7ef4a36fac055d7db2b5e57128f3e98a925
GArdennes <thekevin.afachao@gmail.com>
2024-06-06 19:06:30 +0000

    Task 3

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,19 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should use Utils.calculateNumber to calculate the sum', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    const consoleSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consoleSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+
+    calculateNumberSpy.restore();
+    consoleSpy.restore();
+  });
+});
